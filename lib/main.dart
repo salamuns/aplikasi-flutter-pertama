a0bbf5a2df8feb3b7038376771744f9048dfd67f
salamuns <salamuns@yahoo.com>
2026-09-18 13:04:55 +0700

    Change app name from 'App-Q' to 'App-GAGAL'

--- lib/main.dart
+++ lib/main.dart
@@ -215,7 +215,7 @@ class ProfileScreen extends StatelessWidget {
           ),
           SizedBox(height: 16),
           Text(
-            'Pengguna Oetan Kayoe App-Q',
+            'Pengguna Oetan Kayoe App-GAGAL',
             style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
           ),
           Text('user@oetankayoe.com', style: TextStyle(color: Colors.grey)),
